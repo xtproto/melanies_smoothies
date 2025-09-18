307c018c215357d5e62b9c7661cc4692b46c48d1
xtproto <xtproto@gmail.com>
2025-09-18 18:06:43 +1200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
 # Import python packages
-import streamlit as st
-from snowflake.snowpark.functions import col
 import requests
 import pandas as pd
+import streamlit as st
+from snowflake.snowpark.functions import col
+
 
 
 # Write directly to the app
